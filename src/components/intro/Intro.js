93994cfa3ff7c7ead1fba66ebf1831334829e2e9
Gabriel Ribeiro <garri.ribeiro@gmail.com>
2020-02-24 14:35:43 -0300

    [GAR4] Structure new gatsby project to use paradigma (#5)
    * struct first components, add Home
    
    * remove layout.css and added Normalize component
    
    * add styled-components in project
    
    * start to create theme and home page
    - Create config.js to theme
    - Add reset css to styled- componentes
    - Start to style Home
    
    * add gatsby config to add fonts
    
    * finish garri.dev mvp home
    - add fonts
    - create title
    - create subtitle
    - add coconoutMilk color
    - change metadata gatsby
    
    * remove paradigma-shift html templte
    
    * move app to root folder

---
+++ src/components/intro/Intro.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Wrapper, Title, Subtitle } from './style'
+const Intro = () => {
+    return (
+        <Wrapper>
+           <Title>Hi, I'm Garri.</Title>
+           <Subtitle>Software Engineer at QuintoAndar</Subtitle>
+        </Wrapper>
+    )
+}
+
+export default Intro
---
+++ src/components/layout/Layout.js
@@ -0,0 +1,45 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import React from "react"
+import PropTypes from "prop-types"
+import { useStaticQuery, graphql } from "gatsby"
+import styled from 'styled-components';
+
+import Header from "../header/Header"
+
+const Wrapper = styled.div`
+  height: 100%;
+  display: 'flex';
+  flexDirection: "column";
+`
+const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
+  return (
+    <>
+      {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
+      <Wrapper>
+        <main>{children}</main>
+      </Wrapper>
+    </>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
